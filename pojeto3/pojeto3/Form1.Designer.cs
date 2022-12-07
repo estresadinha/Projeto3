199966d3c3d81c870ca5e3f52fd0b1cfe056bfe4
estresadinha <120022356+estresadinha@users.noreply.github.com>
2022-12-07 10:45:40 -0300

    Add files via upload

---
+++ pojeto3/pojeto3/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace pojeto3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botao1 = new System.Windows.Forms.Button();
+            this.botao2 = new System.Windows.Forms.Button();
+            this.borao3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // botao1
+            // 
+            this.botao1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.botao1.Location = new System.Drawing.Point(138, 41);
+            this.botao1.Name = "botao1";
+            this.botao1.Size = new System.Drawing.Size(99, 40);
+            this.botao1.TabIndex = 0;
+            this.botao1.Text = "traduzir";
+            this.botao1.UseVisualStyleBackColor = false;
+            this.botao1.Click += new System.EventHandler(this.botao1_Click);
+            // 
+            // botao2
+            // 
+            this.botao2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.botao2.Location = new System.Drawing.Point(322, 40);
+            this.botao2.Name = "botao2";
+            this.botao2.Size = new System.Drawing.Size(89, 52);
+            this.botao2.TabIndex = 1;
+            this.botao2.Text = "traduzir";
+            this.botao2.UseVisualStyleBackColor = false;
+            this.botao2.Click += new System.EventHandler(this.botao2_Click);
+            // 
+            // borao3
+            // 
+            this.borao3.BackColor = System.Drawing.Color.Blue;
+            this.borao3.Location = new System.Drawing.Point(507, 42);
+            this.borao3.Name = "borao3";
+            this.borao3.Size = new System.Drawing.Size(75, 39);
+            this.borao3.TabIndex = 2;
+            this.borao3.Text = "traduzir";
+            this.borao3.UseVisualStyleBackColor = false;
+            this.borao3.Click += new System.EventHandler(this.borao3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "dog";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(361, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "cat";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(519, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "boy";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.borao3);
+            this.Controls.Add(this.botao2);
+            this.Controls.Add(this.botao1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botao1;
+        private System.Windows.Forms.Button botao2;
+        private System.Windows.Forms.Button borao3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ pojeto3/pojeto3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pojeto3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void botao1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "cachorro";
+        }
+
+        private void botao2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "gato";
+        }
+
+        private void borao3_Click(object sender, EventArgs e)
+        {
+            label3.Text = "menino";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
